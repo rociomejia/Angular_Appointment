f1673ee5c57ab220cf7704a08d43a81cae17108d
Rociomejia <chiocjava@yahoo.es>
2025-08-22 14:23:16 -0600

    add and remove working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,12 @@
 import { Component } from '@angular/core';
 import { Appointment} from './models/appointment';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-   newAppointmentTitle : string= "";
-  newAppointmentDate: Date = new Date();
-
-  appointments: Appointment[] = []
-
-  addAppointment()
-  {
-     if (this.newAppointmentTitle.trim().length && this.newAppointmentDate) 
-    {
-      let newAppointment: Appointment = {
-        id: Date.now(),
-        title: this.newAppointmentTitle,
-        date: this.newAppointmentDate
-      }
-      this.appointments.push(newAppointment)
-
-      this.newAppointmentDate=new Date();
-      this.newAppointmentTitle="";
-
-      localStorage.setItem("appointments",JSON.stringify(this.appointments))    
-    }
-  }
-  
-  deleteAppointment(index: number){
-      this.appointments.splice(index,1)
-      localStorage.setItem("appointments",JSON.stringify(this.appointments))
-  }
+ title='appointment-app'
 }
--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Appointment } from '../models/appointment';
 
 @Component({
   selector: 'app-appointment-list',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./appointment-list.component.css']
 })
 export class AppointmentListComponent {
+   newAppointmentTitle: string= "";   
+   newAppointmentDate: Date= new Date();
+
+   appointments: Appointment[]=[]
+   
+   AddAppointment(){
+    
+    if (this.newAppointmentTitle.trim().length && this.newAppointmentDate)
+    {
+      let newAppointment : Appointment= {
+        id: Date.now(),
+        title:this.newAppointmentTitle,
+        date:this.newAppointmentDate
+      }
+      this.appointments.push(newAppointment)
+      this.newAppointmentDate=new Date();
+      this.newAppointmentTitle="";
+      
+      localStorage.setItem("appointments",JSON.stringify(this.appointments)) 
+    }
+}
+
+  deleteAppointment(index: number){
+      this.appointments.splice(index,1)
+      localStorage.setItem("appointments",JSON.stringify(this.appointments))
+  }
 
 }
